b3f89f2d03541f4cc870a8eff6092c992b08fdc2
Steve Buckingham <steve.buckingham@contentful.com>
2021-02-15 08:51:57 +0100

    Default pagination to 150
    The Intercom 2.0 API now supports 150 entries returned per call for Pagination objects.
    
    https://developers.intercom.com/intercom-api-reference/v2.0/reference#pagination

--- tap_intercom/sync.py
+++ tap_intercom/sync.py
@@ -166,7 +166,7 @@ def sync_endpoint(client, #pylint: disable=too-many-branches
     page = 1
     offset = 0
     # Default per_page limit is 50, max is 60
-    limit = endpoint_config.get('batch_size', 60)
+    limit = endpoint_config.get('batch_size', 150)
     total_records = 0
 
     # Check scroll_type to determine if to use Scroll API
